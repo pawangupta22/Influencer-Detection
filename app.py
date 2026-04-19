e5986d21b2c69c79bd7c41b649a34008486d0aad
pawangupta22 <pawangupta1923@gmail.com>
2026-04-19 11:00:46 +0530

    new data set

--- app.py
+++ app.py
@@ -1,42 +1,61 @@
 import streamlit as st
 import joblib
 
+# Load model
 model = joblib.load("model.pkl")
 le = joblib.load("encoder.pkl")
 
-st.title("Influencer Classification System")
+st.set_page_config(page_title="Influencer Detection")
 
-st.write("Enter user details")
+st.title("Influencer Detection System")
+
+st.write("Enter profile details")
 
 # Inputs
 followers = st.number_input("Followers", min_value=1)
 following = st.number_input("Following", min_value=0)
 posts = st.number_input("Posts", min_value=0)
-views = st.number_input("Views", min_value=0)
+avg_posts_per_day = st.number_input("Avg Posts Per Day", min_value=0.0)
 
-likes = st.number_input("Likes", min_value=0)
-comments = st.number_input("Comments", min_value=0)
-shares = st.number_input("Shares", min_value=0)
+avg_views = st.number_input("Avg Views Per Post", min_value=0)
+avg_likes = st.number_input("Avg Likes Per Post", min_value=0)
+avg_comments = st.number_input("Avg Comments Per Post", min_value=0)
+avg_shares = st.number_input("Avg Shares Per Post", min_value=0)
 
-account_age_days = st.number_input("Account Age (days)", min_value=1)
+account_age = st.number_input("Account Age (months)", min_value=1)
 
 # Prediction
 if st.button("Predict"):
 
-    # average engagement
-    avg_engagement = (likes + comments + shares) / 3
-
-    # engagement rate
-    engagement_rate = (likes + comments + shares) / followers
+    engagement_rate = (avg_likes + avg_comments + avg_shares) / followers
+    views_ratio = avg_views / followers
 
     data = [[
-        followers, following, posts, views,
-        avg_engagement, shares, account_age_days, engagement_rate
+        followers,
+        following,
+        posts,
+        avg_posts_per_day,
+        avg_views,
+        avg_likes,
+        avg_comments,
+        avg_shares,
+        account_age,
+        engagement_rate,
+        views_ratio
     ]]
 
     pred = model.predict(data)
-    result = le.inverse_transform(pred)
+    result = le.inverse_transform(pred)[0]
 
-    st.write("Average Engagement:", int(avg_engagement))
+    # Influencer or Not
+    if result in ["Real Influencer", "Growing Influencer"]:
+        status = "Influencer"
+    else:
+        status = "Not Influencer"
+
+    # Output
+    st.subheader("Result")
+    st.write("User Type:", result)
+    st.write("Status:", status)
     st.write("Engagement Rate:", round(engagement_rate, 4))
-    st.write("User Type:", result[0])
+    st.write("Views Ratio:", round(views_ratio, 4))
